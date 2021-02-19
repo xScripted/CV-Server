e24d93b5f57249127f871263fa48a6c55c9c63ac
unknown <miquel97bdn@gmail.com>
2021-02-19 17:53:37 +0100

    test

--- server.js
+++ server.js
@@ -3,13 +3,12 @@ const app = express()
 const port = 80
 
 
-app.use(express.static(__dirname + '/CV'));
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+app.get('/test', (req, res) => {
   res.send('Hello World!!!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://www.miqueltoran.com:${port}`)
-  console.log(__dirname + '/CV');
 })
